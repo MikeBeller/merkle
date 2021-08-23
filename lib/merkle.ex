c3c17504d4719451d184e0907f35646c59a7eb25
Mike Beller <mike@beller.tech>
2021-08-23 15:33:39 -0400

    Getting close to a working proof

--- lib/merkle.ex
+++ lib/merkle.ex
@@ -1,9 +1,11 @@
 defmodule Merkle do
+  use Bitwise
+
   @moduledoc """
   Implementation of a Merkle tree
   """
 
-  defstruct [:root, :blocks]
+  defstruct [:root, :blocks, :height]
 
   @leaf_salt <<0>>
   @node_salt <<1>>
@@ -14,7 +16,7 @@ defmodule Merkle do
     |> Base.encode16(case: :lower)
   end
 
-  def build_leaf(data) do
+  defp build_leaf(data) do
       %Merkle.Node{
         hash: hash(@leaf_salt <> data),
         children: [],
@@ -25,30 +27,24 @@ defmodule Merkle do
     new([@default_data])
   end
 
-  #def new([first]) do
-  #  %Merkle{
-  #    root: build_leaf(first),
-  #    blocks: [first],
-  #  }
-  #end
-
   def new(blocks) when is_list(blocks) do
     ln = length(blocks)
     ht = ceil(:math.log2(ln))
-    full_ln = Bitwise.bsl(1, ht)
+    full_ln = 1 <<< ht
     # pad out the blocks to length full_ln
     all_blocks = blocks ++ List.duplicate("", full_ln - ln)
     %Merkle{
       root: build_tree(full_ln, all_blocks),
       blocks: all_blocks,
+      height: ht,
     }
   end
 
-  def build_tree(1, [data]) do
+  defp build_tree(1, [data]) do
     build_leaf(data)
   end
 
-  def build_tree(n, blocks) when rem(n, 2) == 0 do
+  defp build_tree(n, blocks) when rem(n, 2) == 0 do
     c = div(n, 2)
     [left, right] = Enum.chunk_every(blocks, c)
     lt = build_tree(c, left)
@@ -57,5 +53,31 @@ defmodule Merkle do
       hash: hash(@node_salt <> lt.hash <> rt.hash),
       children: [lt, rt],
     }
+  end
+
+  def path(%Merkle{height: ht}, i) when i < (1 <<< ht) do
+    p = Integer.digits(i, 2)
+    List.duplicate(0, ht-length(p)) ++ p
+  end
+
+  def proof(t = %Merkle{root: root}, ind) do
+    pth = path(t, ind)
+    proof(root, pth, [root.hash])
+  end
+
+  def proof(%Merkle.Node{}, [], pf), do: pf
+  def proof(%Merkle.Node{children: children}, [b | rest], pf) do
+    [l, r] = children
+    pf = [l.hash | [r.hash | pf]]
+    proof((if b == 0, do: l, else: r), rest, pf)
+  end
+
+  # not right -- need to reverse order if the path bit was a 1
+  def proven?(_t = %Merkle{}, _ind, pf) do
+    Enum.chunk_every(pf, 3, 2, :discard)
+    |> Enum.reduce(true, fn
+      [lh,rh,nh], v ->
+        v && (hash(@node_salt <> lh <> rh) == nh)
+    end)
   end
 end
--- test/merkle_test.exs
+++ test/merkle_test.exs
@@ -1,28 +1,53 @@
 defmodule MerkleTest do
   use ExUnit.Case
-  #doctest Merkle
+  doctest Merkle
 
   test "New empty tree" do
-    assert %Merkle{root: root, blocks: blocks} = Merkle.new()
+    assert %Merkle{root: root, blocks: blocks, height: ht} = Merkle.new()
+    assert ht == 0
     assert %Merkle.Node{hash: hash, children: []} = root
     assert [""] = blocks
     assert hash == :crypto.hash(:sha256, <<0>> <> "") |> Base.encode16(case: :lower)
   end
 
   test "New one-item tree" do
-    assert %Merkle{root: root, blocks: blocks} = Merkle.new(["foobar"])
+    assert %Merkle{root: root, blocks: blocks, height: ht} = Merkle.new(["foobar"])
+    assert ht == 0
     assert %Merkle.Node{hash: hash, children: []} = root
     assert ["foobar"] = blocks
     assert hash == :crypto.hash(:sha256, <<0>> <> "foobar") |> Base.encode16(case: :lower)
   end
 
   test "Two item tree" do
-    assert %Merkle{root: _root, blocks: blocks} = Merkle.new(["a", "b"])
+    assert %Merkle{root: _root, blocks: blocks, height: ht} = Merkle.new(["a", "b"])
+    assert ht == 1
     assert length(blocks) == 2
   end
 
   test "Three item tree" do
-    assert %Merkle{root: _root, blocks: blocks} = Merkle.new(["a", "b", "c"])
+    assert %Merkle{root: _root, blocks: blocks, height: ht} = Merkle.new(["a", "b", "c"])
+    assert ht == 2
     assert length(blocks) == 4
+  end
+
+  test "path" do
+    t = Merkle.new(["a", "b", "c", "d", "e"])
+    assert Merkle.path(t, 0) == [0, 0, 0]
+    assert Merkle.path(t, 1) == [0, 0, 1]
+    assert Merkle.path(t, 7) == [1, 1, 1]
+  end
+
+  test "proof" do
+    t = Merkle.new(["a", "b", "c"])
+    pf = Merkle.proof(t, 1)
+    trc = t.root.children
+    trcl = hd(trc).children
+    assert pf == Enum.map(trcl ++ trc ++ [t.root], fn x -> x.hash end)
+  end
+
+  test "verify proof" do
+    t = Merkle.new(["a", "b", "c"])
+    pf = Merkle.proof(t, 1)
+    assert Merkle.proven?(t, 1, pf)
   end
 end
