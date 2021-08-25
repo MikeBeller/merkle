f26bb20b318e911b864db5ed18ec224155be809a
Mike Beller <mike@beller.tech>
2021-08-24 20:28:04 -0400

    Refactor Merkle -> Merkle.Tree

---
+++ lib/merkle/tree.ex
@@ -0,0 +1,117 @@
+defmodule Merkle.Tree do
+  use Bitwise
+
+  @moduledoc """
+  Implementation of a Merkle tree data structure
+  """
+
+  defstruct [:root, :blocks, :height]
+  @type t :: %__MODULE__ {
+    root: Merkle.Node.t(),
+    blocks: [binary],
+    height: non_neg_integer(),
+  }
+
+  @leaf_salt <<0>>
+  @node_salt <<1>>
+  @default_data ""
+
+  @type hash_t :: String.t()
+  @spec hash(binary()) :: hash_t()
+  def hash(data) do
+    :crypto.hash(:sha256, data)
+    |> Base.encode16(case: :lower)
+  end
+
+  @spec leaf_hash(binary()) :: hash_t()
+  def leaf_hash(data), do: hash(@leaf_salt <> data)
+
+  @spec node_hash(binary(), binary()) :: hash_t()
+  def node_hash(a, b), do: hash(@node_salt <> a <> b)
+
+  defp build_leaf(data) do
+      %Merkle.Node{
+        hash: leaf_hash(data),
+        children: [],
+      }
+  end
+
+  @spec new() :: Merkle.Tree.t()
+  def new do
+    new([@default_data])
+  end
+
+  @spec new(list) :: Merkle.Tree.t()
+  def new(blocks) when is_list(blocks) do
+    ln = length(blocks)
+    ht = ceil(:math.log2(ln))
+    full_ln = 1 <<< ht
+    # pad out the blocks to length full_ln
+    all_blocks = blocks ++ List.duplicate("", full_ln - ln)
+    %Merkle.Tree{
+      root: build_tree(full_ln, all_blocks),
+      blocks: all_blocks,
+      height: ht,
+    }
+  end
+
+  defp build_tree(1, [data]) do
+    build_leaf(data)
+  end
+
+  defp build_tree(n, blocks) when rem(n, 2) == 0 do
+    c = div(n, 2)
+    [left, right] = Enum.chunk_every(blocks, c)
+    lt = build_tree(c, left)
+    rt = build_tree(c, right)
+    %Merkle.Node{
+      hash: node_hash(lt.hash, rt.hash),
+      children: [lt, rt],
+    }
+  end
+
+  @spec path(non_neg_integer | Merkle.Tree.t(), integer) :: [non_neg_integer()]
+  def path(%Merkle.Tree{height: ht}, i) when i < (1 <<< ht), do: path(ht, i)
+  def path(ht, i) when is_integer(ht) do
+    p = Integer.digits(i, 2)
+    List.duplicate(0, ht-length(p)) ++ p
+  end
+
+  @spec gen_proof(Merkle.Tree.t(), non_neg_integer()) :: Merkle.Proof.t()
+  def gen_proof(t = %Merkle.Tree{root: root}, ind) do
+    pth = path(t, ind)
+    %Merkle.Proof{
+      id: ind,
+      hashes: _gen_proof(root, pth, [root.hash]),
+    }
+  end
+
+  defp _gen_proof(%Merkle.Node{}, [], pf), do: pf
+  defp _gen_proof(%Merkle.Node{children: children}, [p | path], pf) do
+    [l, r] = children
+    case p do
+      0 -> _gen_proof(l, path, [r.hash | pf])
+      1 -> _gen_proof(r, path, [l.hash | pf])
+    end
+  end
+
+  @spec verify_proof(Merkle.Proof.t(), Merkle.Tree.t(), non_neg_integer(), hash_t()) :: boolean()
+  @doc """
+  Verifies that pf correctly proves that xi is the ind-th event in Merkle tree t
+  """
+  def verify_proof(%Merkle.Proof{id: id, hashes: hashes}, t = %Merkle.Tree{}, ind, xi) do
+    pth = path(t, ind) |> Enum.reverse()
+    id == ind && leaf_hash(Enum.at(t.blocks, ind)) == xi && _verify_proof(xi, pth, hashes)
+  end
+
+  defp _verify_proof(curhash, [], [root]) do
+    curhash == root
+  end
+
+  defp _verify_proof(curhash, [p | pth], [h | pf]) do
+    case p do
+      0 -> _verify_proof(node_hash(curhash, h), pth, pf)
+      1 -> _verify_proof(node_hash(h, curhash), pth, pf)
+    end
+  end
+end
---
+++ test/merkle_tree_test.exs
@@ -0,0 +1,62 @@
+defmodule MerkleTreeTest do
+  use ExUnit.Case
+  doctest Merkle.Tree
+
+  test "New empty tree" do
+    assert %Merkle.Tree{root: root, blocks: blocks, height: ht} = Merkle.Tree.new()
+    assert ht == 0
+    assert %Merkle.Node{hash: hash, children: []} = root
+    assert [""] = blocks
+    assert hash == :crypto.hash(:sha256, <<0>> <> "") |> Base.encode16(case: :lower)
+  end
+
+  test "New one-item tree" do
+    assert %Merkle.Tree{root: root, blocks: blocks, height: ht} = Merkle.Tree.new(["foobar"])
+    assert ht == 0
+    assert %Merkle.Node{hash: hash, children: []} = root
+    assert ["foobar"] = blocks
+    assert hash == :crypto.hash(:sha256, <<0>> <> "foobar") |> Base.encode16(case: :lower)
+  end
+
+  test "Two item tree" do
+    assert %Merkle.Tree{root: _root, blocks: blocks, height: ht} = Merkle.Tree.new(["a", "b"])
+    assert ht == 1
+    assert length(blocks) == 2
+  end
+
+  test "Three item tree" do
+    assert %Merkle.Tree{root: _root, blocks: blocks, height: ht} = Merkle.Tree.new(["a", "b", "c"])
+    assert ht == 2
+    assert length(blocks) == 4
+  end
+
+  test "path" do
+    t = Merkle.Tree.new(["a", "b", "c", "d", "e"])
+    assert Merkle.Tree.path(t, 0) == [0, 0, 0]
+    assert Merkle.Tree.path(t, 1) == [0, 0, 1]
+    assert Merkle.Tree.path(t, 7) == [1, 1, 1]
+  end
+
+  test "proof" do
+    t = Merkle.Tree.new(["a", "b", "c"])
+    pf = Merkle.Tree.gen_proof(t, 1)
+    #IO.inspect t
+    #IO.inspect pf
+    [kid_l, kid_r] = t.root.children
+    [gc_l, _gc_r] = kid_l.children
+    assert pf.hashes == Enum.map([gc_l, kid_r, t.root], &(&1.hash))
+  end
+
+  test "verify proof" do
+    t = Merkle.Tree.new(["a", "b", "c"])
+    pf = Merkle.Tree.gen_proof(t, 1)
+    assert Merkle.Tree.verify_proof(pf, t, 1, Merkle.Tree.leaf_hash("b"))
+    assert !Merkle.Tree.verify_proof(pf, t, 1, Merkle.Tree.leaf_hash("x"))
+
+    # check them all
+    assert (t.blocks
+    |> Enum.with_index(fn bl,ind ->
+      Merkle.Tree.verify_proof(Merkle.Tree.gen_proof(t, ind), t, ind, Merkle.Tree.leaf_hash(bl))
+    end) |> Enum.all?())
+  end
+end
